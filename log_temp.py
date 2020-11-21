81d8d7c990348d1f0d60cd29b183a8763e12e9b7
tripod31 <tripod31@hotmail.com>
2020-11-21 10:09:19 +0900

    check already running

--- log_temp.py
+++ log_temp.py
@@ -7,6 +7,7 @@ import sys
 import time
 import os
 import logging
+import subprocess
 
 class Process:
     CONF_FILE=".config/log_temp.conf"
@@ -25,7 +26,29 @@ class Process:
         self.TEMP_FILE=config["settings"]["temp_file"]
         self.INTERVAL_MIN=int(config["settings"]["interval_min"])
 
+    def check_already_running(self):
+        """
+        check if process is already running
+        Returns
+        -------
+            : bool
+        """
+        file_name = os.path.basename(__file__)
+        p1 = subprocess.Popen(["ps", "-ef"], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["grep", file_name], stdin=p1.stdout, stdout=subprocess.PIPE)
+        p3 = subprocess.Popen(["grep", "python3"], stdin=p2.stdout, stdout=subprocess.PIPE)
+        p4 = subprocess.Popen(["wc", "-l"], stdin=p3.stdout, stdout=subprocess.PIPE)
+        p1.stdout.close()
+        p2.stdout.close()
+        p3.stdout.close()
+        output = p4.communicate()[0].decode("utf8").replace('\n','')
+        return True if int(output != 1) else False
+
     def main(self):
+        if self.check_already_running():
+            print("already running.exit.")
+            sys.exit(-1)
+
         os.chdir(os.path.dirname(__file__)) #chdir to script path
         self.read_config()
         logging.basicConfig(
